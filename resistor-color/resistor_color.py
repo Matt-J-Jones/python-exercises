8b76c41612b0e106204c151eed48311f32a896c2
Matt-J-Jones <aratinlondon@gmail.com>
2023-11-07 12:32:33 +0000

    Exercise Complete: Resistor Color

---
+++ resistor-color/resistor_color.py
@@ -0,0 +1,18 @@
+COLORS_DICT = {
+        'black': 0,
+        'brown': 1,
+        'red': 2,
+        'orange': 3,
+        'yellow': 4,
+        'green': 5,
+        'blue': 6,
+        'violet': 7,
+        'grey': 8,
+        'white': 9
+}
+
+def color_code(color):
+    return COLORS_DICT[color]
+
+def colors():
+    return [*COLORS_DICT.keys()]
